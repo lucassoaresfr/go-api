de324152903d0f5eb062779b0f978d629bae4ca8
lucassoaresfr <lucasclachack@gmail.com>
2026-02-24 14:10:21 -0300

    Metodo router e handler adicionado 	new file:   .gitignore 	modified:   go.mod 	new file:   handler/createOpening.go 	new file:   handler/deleteOpening.go 	new file:   handler/handler.go 	new file:   handler/listOpening.go 	new file:   handler/showOpening.go 	new file:   handler/updateOpening.go 	new file:   main.go 	new file:   router/router.go 	new file:   router/routes.go

---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "github.com/lucassoaresfr/go-api.git/router"
+
+
+func main() {
+	router.Initialize()
+}
---
+++ router/routes.go
@@ -0,0 +1,21 @@
+package router
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/lucassoaresfr/go-api.git/handler"
+)
+
+func initializeRoutes(router *gin.Engine) {
+	v1 := router.Group("/api/v1")
+	{
+		v1.GET("/opening", handler.ShowOpeningHandler)
+
+		v1.POST("/opening", handler.CreateOpeningHandler)
+
+		v1.DELETE("/opening", handler.DeleteOpeningHandler)
+
+		v1.PUT("/opening", handler.UpdateOpeningHandler)
+
+		v1.GET("/openings", handler.ListOpeningHandler)
+	}
+}
